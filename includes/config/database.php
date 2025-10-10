ad583248c9bb5e18705b9da20b5dea32c49494f3
Mateo Pavoni <mateopavoni905@gmail.com>
2025-10-10 00:52:03 -0300

    feat: cree la conexion a la bbdd, añadiendo composer y la dependencia de phpdotenv para leer el .env y obtener mis credenciales

---
+++ includes/config/database.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/../../vendor/autoload.php'; 
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../../");
+$dotenv->load();
+
+function conectarDB() : mysqli {
+    $db = mysqli_connect(
+        $_ENV['DB_HOST'],
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS'],
+        $_ENV['DB_NAME']
+    );
+
+    if (!$db) {
+        die("Error al conectar con la base de datos: " . mysqli_connect_error());
+    }
+
+    return $db;
+}
