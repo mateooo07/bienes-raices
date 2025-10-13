c879d52b79d3461fc92a768c05594f0ba8f1b95a
Mateo Pavoni <mateopavoni905@gmail.com>
2025-10-13 11:33:41 -0300

    feat: agregue validaciones al formulario

--- login.php
+++ login.php
@@ -1,4 +1,24 @@
-<?php 
+<?php
+
+    require "includes/config/database.php";
+
+    $db = conectarDB();
+
+    $errores = [];
+
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
+        $email = mysqli_real_escape_string($db, filter_var($_POST["email"], FILTER_VALIDATE_EMAIL));
+        $password = mysqli_real_escape_string($db, $_POST["password"]);
+
+        if(!$email){
+            $errores[] = "El email no es válido";
+        }
+
+        if(!$password){
+            $errores[] = "El password es obligatorio";
+        }
+    }
+
     require "includes/funciones.php";
     incluirTemplate("header");
 ?>
@@ -7,13 +27,19 @@
     <main class="contenedor seccion contenido-centrado">
         <h1>Iniciar Sesión</h1>
 
-        <form class="formulario">
+        <?php foreach($errores as $error): ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach;?>
+
+        <form class="formulario" method="post">
             <fieldset>
                 <label for="email">E-mail</label>
-                <input type="email" placeholder="Tu Email" id="email">
+                <input type="email" name="email" placeholder="Tu Email" id="email" required>
 
                 <label for="password">Password</label>
-                <input type="password" placeholder="Tu Password" id="telefono">
+                <input type="password" name="password" placeholder="Tu Password" id="telefono" required>
             </fieldset>
             <input type="submit" value="Iniciar sesión" class="boton boton-verde">
         </form>
