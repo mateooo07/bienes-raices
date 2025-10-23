87a61cd3f9a98c711304d193475cd7d1360cbe53
Mateo Pavoni <mateopavoni905@gmail.com>
2025-10-23 19:02:32 -0300

    feat: agregue asociacion de urls a metodos del controlador de Propiedad

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 require_once __DIR__ . "/../includes/app.php";
 use MVC\Router;
+use Controllers\PropiedadController;
 
 $router = new Router();
 
-$router->get("/nosotros", "funcion_nosotros" );
-$router->get("/contacto", "funcion_contacto" );
-$router->get("/tienda_virtual", "funcion_tienda" );
-$router->get("/admin", "funcion_admin" );
+$router->get("/propiedades/actualizar", [PropiedadController::class, "actualizar"] );
+$router->get("/propiedades/crear", [PropiedadController::class, "crear"] );
+$router->get("/admin", [PropiedadController::class, "index"] );
 
 $router->comprobarRutas();
