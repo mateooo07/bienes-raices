5e8ab5fd672226d0798448c86f906821ffeb3ec7
Mateo Pavoni <mateopavoni905@gmail.com>
2025-10-23 20:53:33 -0300

    feat: agregue logica para terminar de convertir el metodo actualizar de Propiedad a MVC

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -22,7 +22,7 @@ class PropiedadController{
     public static function crear(Router $router){
         $propiedad = new Propiedad;
         $vendedores = Vendedor::all();
-        $errores = [];
+        $errores = Propiedad::getErrores();
 
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $propiedad = new Propiedad($_POST["propiedad"]);
@@ -56,7 +56,41 @@ class PropiedadController{
         ]);
     }
 
-    public static function actualizar(){
+    public static function actualizar(Router $router){
+        $id = validarORedireccionar("/admin");
+        $propiedad  = Propiedad::find($id);
+        $vendedores = Vendedor::all();
+        $errores = Propiedad::getErrores();
 
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+        $args = $_POST["propiedad"];
+
+        $propiedad->sincronizar($args);
+
+        $errores = $propiedad->validar();
+
+        $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+
+        if ($_FILES["propiedad"]["tmp_name"]["imagen"]) {
+            $manager = new Image(Driver::class);
+            $imagen = $manager->read($_FILES["propiedad"]["tmp_name"]["imagen"])->cover(800, 600);
+            $propiedad->setImagen($nombreImagen);
+        }
+
+        if (empty($errores)) {
+            if ($_FILES["propiedad"]["tmp_name"]["imagen"]){
+                $imagen->save(CARPETA_IMAGENES . $nombreImagen);
+            }
+            
+            $propiedad->guardar();
+        }
+    }
+
+        $router-> render("propiedades/actualizar", [
+            "propiedad" => $propiedad,
+            "vendedores" => $vendedores,
+            "errores" => $errores
+        ]);
     }
 }
