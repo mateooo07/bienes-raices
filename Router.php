7a77546b413c4e1ad477f960d27426f0830dbb18
Mateo Pavoni <mateopavoni905@gmail.com>
2025-10-23 17:31:33 -0300

    feat: agregue logica al router para que solo las url con una función asignada sean accesible

--- Router.php
+++ Router.php
@@ -2,8 +2,26 @@
 
 namespace MVC;
 class Router {
-    public function __construct()
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn){
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas()
     {
-        
+        $urlActual = $_SERVER["PATH_INFO"];
+        $metodo = $_SERVER["REQUEST_METHOD"];
+
+        if($metodo === "GET"){
+            $fn = $this->rutasGET[$urlActual] ?? null;;
+        }
+
+        if($fn){
+            call_user_func($fn, $this);
+        }else{
+
+        }
     }
 }
--- public/index.php
+++ public/index.php
@@ -3,3 +3,10 @@ require_once __DIR__ . "/../includes/app.php";
 use MVC\Router;
 
 $router = new Router();
+
+$router->get("/nosotros", "funcion_nosotros" );
+$router->get("/contacto", "funcion_contacto" );
+$router->get("/tienda_virtual", "funcion_tienda" );
+$router->get("/admin", "funcion_admin" );
+
+$router->comprobarRutas();
