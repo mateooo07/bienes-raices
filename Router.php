a33345105b6f9904bb756587121e1a27eb11f724
Mateo Pavoni <mateopavoni905@gmail.com>
2025-10-25 01:57:29 -0300

    feat: agregue proteccion a las rutas

--- Router.php
+++ Router.php
@@ -15,13 +15,24 @@ class Router {
 
     public function comprobarRutas()
     {
+        session_start();
+
+        $auth= $_SESSION["login"] ?? null;
+
+        $rutas_protegidas = ["/admin", "/propiedades/crear", "/propiedades/actualizar","/propiedades/eliminar", "/vendedores/crear", "/vendedores/actualizar", "/vendedores/eliminar"];
+
         $urlActual = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
         $metodo = $_SERVER["REQUEST_METHOD"];
 
         if($metodo === "GET"){
             $fn = $this->rutasGET[$urlActual] ?? null;
         } else{
             $fn = $this->rutasPOST[$urlActual] ?? null;
+        }
+
+        if(in_array($urlActual, $rutas_protegidas) && !$auth){
+            header("Location: /");
         }
 
 
