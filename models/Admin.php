2c8039fdf46088f36122fb8d0c68cb65946a3eed
Mateo Pavoni <mateopavoni905@gmail.com>
2025-10-25 01:24:54 -0300

    feat: cree el modelo AdmiN

--- models/Admin.php
+++ models/Admin.php
@@ -3,5 +3,18 @@
 namespace Model;
 
 class Admin extends ActiveRecord{
+    protected static $tabla = "usuarios";
+    protected static $columnas_DB = ["id", "email", "password"];
+
+    public $id;
+    public $email;
+    public $password;
+
+    public function __construct($args = []){
+        $this->id = $args["id"] ?? null;
+        $this->email = $args["email"] ?? null;
+        $this->password = $args["password"] ?? null;
+
+    }
     
 }
